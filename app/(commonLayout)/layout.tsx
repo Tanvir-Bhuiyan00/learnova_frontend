a8b4a877ff516286306942bd30962940bbffb9e2
Tanvir Bhuiyan <tanvirbhuiyan2001@gmail.com>
2026-07-19 20:07:59 +0600

    Refactor CommonLayout component and add CommonHomePage and NotFound pages

--- app/(commonLayout)/layout.tsx
+++ app/(commonLayout)/layout.tsx
@@ -1,3 +1,7 @@
-export default function CommonLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>
+export default function CommonLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
 }
